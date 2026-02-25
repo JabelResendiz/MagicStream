e9aaea4d89f577c3ad8aeaec8c950dac2a4ed070
JabelResendiz <jabelresendiz00@gmail.com>
2026-02-24 22:18:54 -0500

    feat(models): create Movie model with IMDb ID,  genres, ranking, and validation tags

--- server/magicStreamMoviesServer/main.go
+++ server/magicStreamMoviesServer/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
-	fmt.Println("Hello, go world!")
+	
+	// Create a new Gin router with defautl middleware
+	router:= gin.Default()
+
+	// Endpoint GET /hello
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, MagicStreamMovies")
+	})
+
+	// Start server on port 8080
+	if err:= router.Run(":8080"); err!= nil {
+		fmt.Println("Failed to start server", err)
+	}
 }
